574f55c79b2c55ce527554a3e50fcc19c5375f13
vakryss <vakryss@gmail.com>
2026-01-26 19:06:53 +0800

    Update supabase.js

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://jpjybbldgtwlmisjnzkc.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwanliYmxkZ3R3bG1pc2puemtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzNTcxODksImV4cCI6MjA4NDkzMzE4OX0.qTU0G7PM5xlIr5ozNTob0glv8w_iQvTeldrGNuhczkw";
 
 export const supabase = createClient(
   SUPABASE_URL,
